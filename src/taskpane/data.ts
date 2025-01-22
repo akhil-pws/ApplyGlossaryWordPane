73f0265b8f9c28f5eefe360b0445de94606e297b
akhil-pws <akhil.a@pacewisdom.com>
2025-01-22 17:16:04 +0530

    version added

--- src/taskpane/data.ts
+++ src/taskpane/data.ts
@@ -1,3 +1,4 @@
 const dataUrl='https://plsdevapp.azurewebsites.net'
 const storeUrl='https://linkwordplugin-aphgcwcgbfdqeccs.eastus-01.azurewebsites.net'
-export {dataUrl,storeUrl};
+const versionLink='1.0.1'
+export {dataUrl,storeUrl,versionLink};
