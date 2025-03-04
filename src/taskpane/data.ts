841b72076b1d035c69002ca81645e9a48138b105
akhil-pws <akhil.a@pacewisdom.com>
2025-03-04 09:36:31 +0530

    feat: enhance table handling and formatting in document fetching

--- src/taskpane/data.ts
+++ src/taskpane/data.ts
@@ -1,4 +1,4 @@
 const dataUrl='https://plsdevapp.azurewebsites.net'
 const storeUrl='https://linkwordplugin-aphgcwcgbfdqeccs.eastus-01.azurewebsites.net'
-const versionLink='1.0.4'
+const versionLink='1.0.5'
 export {dataUrl,storeUrl,versionLink};
