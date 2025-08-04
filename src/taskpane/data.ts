314786e5eb8ce94a5e4f5dcd7a22ad76fa10ad0c
akhil-pws <akhil.a@pacewisdom.com>
2025-08-04 15:23:37 +0530

    prompt versioning

--- src/taskpane/data.ts
+++ src/taskpane/data.ts
@@ -1,4 +1,4 @@
 const dataUrl='https://plsdevapp.azurewebsites.net'
 const storeUrl='https://akhil-pws.github.io/ApplyGlossaryWordPane'
-const versionLink='2.0.1'
+const versionLink='2.0.2'
 export {dataUrl,storeUrl,versionLink};
