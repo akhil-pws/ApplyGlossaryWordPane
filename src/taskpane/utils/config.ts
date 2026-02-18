5710087f9afab81e5da7288b8b37ee949419ffa2
akhil-pws <akhil.a@pacewisdom.com>
2026-02-18 16:52:10 +0530

    Update dataUrl in config to point to the production server

--- src/taskpane/utils/config.ts
+++ src/taskpane/utils/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    dataUrl: 'https://localhost:7167',
+    dataUrl: 'https://trialassurelinkmvdev-hmb9eybyakf3gbb0.eastus2-01.azurewebsites.net',
     storeUrl: 'https://linkwordplugin-aphgcwcgbfdqeccs.eastus-01.azurewebsites.net',
     version: '2.4.4'
 };
