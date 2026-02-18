30e14452b1720f84fb3212d6b132157d8e5747ee
akhil-pws <128016518+akhil-pws@users.noreply.github.com>
2026-02-18 15:37:28 +0530

    Update config.ts

--- src/taskpane/utils/config.ts
+++ src/taskpane/utils/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    dataUrl: 'https://localhost:53245',
+    dataUrl: 'https://localhost:7167',
     storeUrl: 'https://linkwordplugin-aphgcwcgbfdqeccs.eastus-01.azurewebsites.net',
     version: '2.4.4'
 };
